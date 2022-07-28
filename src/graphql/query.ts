1d9a6d2951fa7207fc76c513fb952e724ec16631
SurinderB <surinder22.batti@gmail.com>
2022-07-28 18:38:40 +0530

    query.ts module

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -1,5 +1,27 @@
 import { gql } from '@apollo/client';
 
+export const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+export const GET_LOCATION = gql`
+  query GetLocations($id: ID!) {
+    location(id: $id) {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
 export const GET_TODOS = gql`
   query GetTodos {
     todos {
